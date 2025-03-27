8b7168fde0d208ec1f14a3f4e39a418c1e0ea4ac
Krathor2212 <phome1625@gmail.com>
2025-03-28 00:59:36 +0530

    error debugging

--- Client/app/(tabs)/home.tsx
+++ Client/app/(tabs)/home.tsx
@@ -92,7 +92,7 @@ export default function HomeScreen() {
                           onPress={() =>
                             router.push({
                               pathname: "/topic",
-                              params: { id: topic.id, name: topic.name, description: topic.description },
+                              params: { title:chapter.name,id: topic.id, name: topic.name, description: topic.description },
                             })
                           }
                           left={(props) => <List.Icon {...props} icon="file-document" />}
