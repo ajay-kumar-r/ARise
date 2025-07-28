815db38455a695edc02892b5e23d2ab160fbb396
Krathor2212 <phome1625@gmail.com>
2025-07-28 13:56:20 +0530

    Updated Members Info

--- Client/src/data/contributorsData.ts
+++ Client/src/data/contributorsData.ts
@@ -1,16 +1,23 @@
 export const contributorsData = {
   projectLeads: [
     {
+      name: 'Purushothaman S',
+      roll: '2022115109',
+      gmail: 'purushothamans22122004@gmailo.com',
+      linkedin: 'www.linkedin.com/in/purushothaman-srinivasan-a79423258',
+      image: 'image_url_here-drive-link',
+    },
+    {
       name: 'Ajay Kumar',
-      roll: '21BCE9999',
+      roll: '2022115086',
       gmail: 'ajay.kumar@domain.com',
       linkedin: 'linkedin.com/in/ajay-kumar-r2004',
       image: '',
     },
     {
-      name: 'Sneha Rao',
-      roll: '21BIT8888',
-      gmail: 'sneha.rao@domain.com',
+      name: 'Hariharan P',
+      roll: '2022115029',
+      gmail: 'hariharan@domain.com',
       linkedin: 'linkedin.com/in/sneharao',
       image: 'https://thisurldoesnotexist.com/fake.jpg',
     },
@@ -18,14 +25,14 @@ export const contributorsData = {
   team3D: {
     heads: [
       {
-        name: 'Rahul Singh',
+        name: 'Mytreyan',
         roll: '21BCE1001',
         gmail: 'rahul.singh@domain.com',
         linkedin: 'linkedin.com/in/rahulsingh',
         image: '',
       },
       {
-        name: 'Rahul Singh',
+        name: 'Shashank Gopalan',
         roll: '21BCE1001',
         gmail: 'rahul.singh@domain.com',
         linkedin: 'linkedin.com/in/rahulsingh',
@@ -34,22 +41,204 @@ export const contributorsData = {
     ],
     members: [
       {
-        name: 'Priya Das',
-        roll: '21BIT1102',
-        gmail: 'priya.das@domain.com',
-        linkedin: 'linkedin.com/in/priyadas',
+        name: 'Boobalan P',
+        roll: '2022115104',
+        gmail: 'boobalanvk254@gmail.com',
+        linkedin: 'linkedin.com/in/boobalanvk254',
         image: '',
       },
       {
-        name: 'Karan Patel',
-        roll: '21BCE1203',
-        gmail: 'karan.patel@domain.com',
-        linkedin: 'linkedin.com/in/karanpatel',
+        name: 'Suganth S A',
+        roll: '2022115308',
+        gmail: 'sasuganth@gmail.com',
+        linkedin: 'linkedin.com/in/suganth-s-a-',
         image: '',
       },
+
     ],
   },
-  teamAR: { heads: [], members: [] },
-  teamAuthoring: { heads: [], members: [] },
-  teamEContent: { heads: [], members: [] },
+  teamAR: { 
+    heads: [{
+        name: 'Shashank Narayan Ramnarendran',
+        roll: '2022115134',
+        gmail: 'shashanknarayanram@gmail.com',
+        linkedin: 'linkedin.com/in/shashank-narayan-ram',
+        image: '',
+      },
+      {
+        name: 'Bavishyaa Sriraman',
+        roll: '2022115130',
+        gmail: 'bavishyaa1994@gmail.com',
+        linkedin: 'linkedin.com/in/bavishyaa-sriraman-5b322a331',
+        image: '',
+      },
+  ], 
+  members: [
+      {
+      name: 'Shevaniga S',
+      roll: '2022115008',
+      gmail: 'shevanigasri@gmail.com',
+      linkedin: 'linkedin.com/in/shevaniga-sridhar-99ba61251',
+      image: '',
+    },
+    {
+    name: 'R.Yogasimman',
+    roll: '2022115125',
+    gmail: 'Yogasimmanravisagar@gmail.com',
+    linkedin: 'linkedin.com/in/yogasimman',
+    image: '',
+    },
+    {
+      name: 'Preethi B',
+      roll: '2022115305',
+      gmail: 'preethib163@gmail.com',
+      linkedin: 'linkedin.com/in/preethi-b-7302652b2',
+      image: '',
+    },
+    {
+      name: 'Mohamed Afzar N',
+      roll: '2022115301',
+      gmail: 'afzar0111@gmail.com',
+      linkedin: 'linkedin.com/in/mohamedafzar',
+      image: '',
+    },
+    {
+      name: 'Vignesh S',
+      roll: '2022115093',
+      gmail: 'vigneshsekar339@gmail.com',
+      linkedin: 'linkedin.com/in/vignesh-sekar-ba47972a1',
+      image: '',
+    },
+    {
+      name: 'Vishal Sanjeevi AS',
+      roll: '2022115127',
+      gmail: 'vishalsanjeevi2004@gmail.com',
+      linkedin: 'linkedin.com/in/vishal-sanjeevi-42002a286',
+      image: '',
+    },
+    {
+      name: 'Kesav Kumar J',
+      roll: '2022115088',
+      gmail: 'kesav25155@gmail.com',
+      linkedin: 'linkedin.com/in/kesavkumarj',
+      image: '',
+    },
+    {
+      name: 'Manish M',
+      roll: '2022115080',
+      gmail: 'manish.muru07@gmail.com',
+      linkedin: 'linkedin.com/in/manish-murugan-700354320',
+      image: '',
+    },
+    {
+      name: 'Neeraj Emanuel N',
+      roll: '2022115122',
+      gmail: 'neerajemanuel@gmail.com',
+      linkedin: 'linkedin.com/in/neeraj-emanuel-428423242',
+      image: '',
+    },
+    {
+      name: 'Gowthami Kandhasamy',
+      roll: '2022115026',
+      gmail: 'gowthamikandhasamy1@gmail.com',
+      linkedin: 'linkedin.com/in/gowthami-kandhasamy-b345bb343',
+      image: '',
+    },
+    {
+      name: 'Prasitaa K',
+      roll: '2022115014',
+      gmail: 'prasitaa12a3@gmail.com',
+      linkedin: 'linkedin.com/in/Prasitaa Kannadasan',
+      image: '',
+    },
+    {
+      name: 'Razeen N',
+      roll: '2022115091',
+      gmail: 'razeen112004@gmail.com',
+      linkedin: 'linkedin.com/in/razeen-n-15b8b2271',
+      image: '',
+    },
+    {
+      name: 'Pavithranraj',
+      roll: '2022115306',
+      gmail: 'pavithran.prabhu47@gmail.com',
+      linkedin: 'linkedin.com/in/pavithranraj',
+      image: '',
+    },
+    {
+      name: 'Rithiswaran J',
+      roll: '2022115310',
+      gmail: 'rithiswaran040105@gmail.com',
+      linkedin: 'linkedin.com/in/rithis-040105j',
+      image: '',
+    },
+
+
+  ] },
+  teamAuthoring: { 
+  heads: [
+      {
+        name: 'Jothiruban M',
+        roll: '2022115096',
+        gmail: 'jothiruban1512@gmail.com',
+        linkedin: 'linkedin.com/in/jothiruban',
+        image: '',
+      },
+
+  ],
+   members: [
+    {
+      name: 'Arun Karthick s',
+      roll: '2022115082',
+      gmail: 'arunsaran6666@gmail.com',
+      linkedin: 'linkedin.com/in/arun-karthick-saravanan-4958612a7',
+      image: '',
+    },
+    {
+    name: 'Abhinav MS',
+    roll: '2022115039',
+    gmail: '18abhinavms@gmail.com',
+    linkedin: 'linkedin.com/in/ms-abhinav',
+    image: '',
+    },
+    {
+      name: 'Sree Ram T R',
+      roll: '2022115108',
+      gmail: 'sreeramtrsd@gmail.com',
+      linkedin: 'linkedin.com/in/sreeramtr',
+      image: '',
+    },
+    {
+      name: 'Revathi R T',
+      roll: '2022115111',
+      gmail: 'revathi.r.t135@gmail.con',
+      linkedin: 'linkedin.com/in/revathi-thiruvasagan',
+      image: '',
+    },
+    {
+      name: 'Sujith A',
+      roll: '2022115312',
+      gmail: 'sujith.2k5@gmail.com',
+      linkedin: 'linkedin.com/in/sujith-a-44223a320',
+      image: '',
+    },
+    {
+      name: 'Kaviya G',
+      roll: '2022115106',
+      gmail: 'kaviya080904@gmail.com',
+      linkedin: 'linkedin.com/in/kaviya-g-110a4a298',
+      image: '',
+    },
+   ] },
+  teamEContent: { 
+    heads: [
+      {
+        name: 'VINITH V',
+        roll: '2022115083',
+        gmail: 'msvinith1234@gmail.com',
+        linkedin: 'linkedin.com/in/vinith-v-8abb56320',
+        image: '',
+      },
+
+    ], members: [] },
 };
